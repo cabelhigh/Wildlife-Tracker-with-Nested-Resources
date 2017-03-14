0e514f94728e25abef51888572ca5ce73cde9ed4
cabelhigh <cabelhigh@yahoo.com>
2017-03-14 16:42:29 -0700

    initial commit

---
+++ db/migrate/20170314230242_create_sightings.rb
@@ -0,0 +1,13 @@
+class CreateSightings < ActiveRecord::Migration
+  def change
+    create_table :sightings do |t|
+      t.string :date
+      t.string :time
+      t.string :latitude
+      t.string :longitude
+      t.references :animal, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
